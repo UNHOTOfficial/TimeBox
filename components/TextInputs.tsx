86b43ca6686d8b6671497e6003fb498ae8031348
UNHOT <Serverings@protonmail.com>
2023-03-27 09:26:39 +0430

    Highlight current hour.

--- components/TextInputs.tsx
+++ components/TextInputs.tsx
@@ -1,3 +1,4 @@
+import { getTime } from "@/services/GetDate";
 import React from "react";
 
 export default function TextInputs({
@@ -9,7 +10,39 @@ export default function TextInputs({
   handleInputChange,
   formData,
 }: any) {
-  return (
+  const currentHour = getTime();
+
+  return currentHour === hour ? (
+    <ul className="flex">
+      <li>
+        <span className="bg-blue-100 w-8 border-y-2 border-slate-800 text-gray-900 text-sm rounded-x-sm block p-2.5 first:border-t-0">
+          {hour}
+        </span>
+      </li>
+
+      <li>
+        <input
+          type="text"
+          id="top2"
+          name={name}
+          onChange={handleInputChange}
+          defaultValue={formData[name]}
+          className="bg-blue-100 w-full border-2 border-slate-800 text-gray-900 text-sm block py-2.5 first:border-t-0"
+        />
+      </li>
+
+      <li>
+        <input
+          type="text"
+          id="top1"
+          name={name + 0.5}
+          onChange={handleInputChange}
+          defaultValue={formData[name + 0.5]}
+          className="bg-blue-100 w-full border-y-2 border-slate-800 text-gray-900 text-sm block py-2.5 first:border-t-0"
+        />
+      </li>
+    </ul>
+  ) : (
     <ul className="flex">
       <li>
         <span className="w-8 border-y-2 border-slate-800 text-gray-900 text-sm rounded-x-sm block p-2.5 first:border-t-0">
