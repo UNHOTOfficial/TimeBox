fb8a593298bae14a0cdfee863fe3713b90a6f07e
UNHOT <Serverings@protonmail.com>
2023-03-27 13:38:54 +0430

    Add edit.

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -1,20 +1,25 @@
 "use client";
 
+import { handleEdit, handleReset, handleSave } from "@/services/Handlers";
 import React from "react";
 
 export default function Buttons({ action }: any) {
-  const handleReset = () => {
-    localStorage.clear();
-    location.reload();
-  };
   return (
     <div className="flex flex-col w-full mt-8">
       <button
-        type="submit"
-        disabled
+        type="button"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        onClick={handleSave}
       >
         Save
+      </button>
+
+      <button
+        type="button"
+        className="text-white bg-purple-500 hover:bg-purple-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-500 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800"
+        onClick={handleEdit}
+      >
+        Edit
       </button>
 
       <button
