27046ccfc8e15be6af2810a277909ef874006bdd
UNHOT <Serverings@protonmail.com>
2023-03-27 14:06:55 +0430

    Handlers services.

--- services/Handlers.ts
+++ services/Handlers.ts
@@ -4,6 +4,18 @@ export const handleClick = (e: any) => {
   }
 };
 
+export const handleSave = () => {
+  const inputs = document.querySelectorAll("input");
+  const textAreas = document.querySelectorAll("textarea");
+  inputs.forEach((input) => {
+    input.disabled = true;
+  });
+  textAreas.forEach((textArea) => {
+    textArea.disabled = true;
+  });
+  localStorage.setItem("Status", "save");
+};
+
 export const handleEdit = () => {
   const inputs = document.querySelectorAll("input");
   const textAreas = document.querySelectorAll("textarea");
@@ -15,18 +27,22 @@ export const handleEdit = () => {
   });
 };
 
-export const handleSave = () => {
-  const inputs = document.querySelectorAll("input");
-  const textAreas = document.querySelectorAll("textarea");
-  inputs.forEach((input) => {
-    input.disabled = true;
-  });
-  textAreas.forEach((textArea) => {
-    textArea.disabled = true;
-  });
-};
-
 export const handleReset = () => {
   localStorage.clear();
   location.reload();
 };
+
+// export function handleInputChange(
+//   event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+//   setFormData:any,
+//   prevFormData:any,
+//   formData:any
+// ): void {
+//   const { name, value } = event.target;
+//   setFormData((prevFormData:any) => ({
+//     ...prevFormData,
+//     [name]: value,
+//   }));
+//   const data = JSON.stringify(formData);
+//   localStorage.setItem("BrainDump", data);
+// }
