587b729455486f0a183b74f3180d30df957d83b7
UNHOT <Serverings@protonmail.com>
2023-03-27 10:17:30 +0430

    Minor fixes.

--- app/page.tsx
+++ app/page.tsx
@@ -12,23 +12,23 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <div>
-    <form className="columns-4 gap-4 flex m-4 font-roboto max-w-xl xl:mx-auto">
-      <div className="flex flex-col w-full mt-9 items-center">
-        <TimeBox />
-        <TopPriorities />
-        <BrainDump />
-        <Buttons />
-      </div>
-      <div className="w-full mt-4">
-        <Date />
-        <div className="font-semibold flex justify-around ml-8">
-          <span>:00</span>
-          <span>:30</span>
+      <form className="columns-4 gap-4 flex m-4 font-roboto max-w-xl xl:mx-auto">
+        <div className="flex flex-col w-full mt-9 items-center">
+          <TimeBox />
+          <TopPriorities />
+          <BrainDump />
+          <Buttons />
         </div>
-        <Hours />
-      </div>
-    </form>
-      <Footer/>
+        <div className="w-full mt-4">
+          <Date />
+          <div className="font-semibold flex justify-around ml-8">
+            <span>:00</span>
+            <span>:30</span>
+          </div>
+          <Hours />
+        </div>
+      </form>
+      <Footer />
     </div>
   );
 }
