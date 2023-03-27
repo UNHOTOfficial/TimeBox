2f786a40a427014c60496dfe9f1503e3cc923252
UNHOT <Serverings@protonmail.com>
2023-03-27 11:49:44 +0430

    Add padding.

--- components/TextInputs.tsx
+++ components/TextInputs.tsx
@@ -30,18 +30,18 @@ export default function TextInputs({
           name={name}
           onChange={handleInputChange}
           defaultValue={formData[name]}
-          className="bg-blue-100 w-full border-2 border-slate-800 text-gray-900 text-sm block py-2.5 first:border-t-0"
+          className="bg-blue-100 w-full border-2 border-slate-800 text-gray-900 text-sm block p-2.5 first:border-t-0"
         />
       </li>
 
       <li>
         <input
           type="text"
-          id={name+0.5}
+          id={name + 0.5}
           name={name + 0.5}
           onChange={handleInputChange}
           defaultValue={formData[name + 0.5]}
-          className="bg-blue-100 w-full border-y-2 border-slate-800 text-gray-900 text-sm block py-2.5 first:border-t-0"
+          className="bg-blue-100 w-full border-y-2 border-slate-800 text-gray-900 text-sm block p-2.5 first:border-t-0"
         />
       </li>
     </ul>
@@ -60,18 +60,18 @@ export default function TextInputs({
           name={name}
           onChange={handleInputChange}
           defaultValue={formData[name]}
-          className="w-full border-2 border-slate-800 text-gray-900 text-sm block py-2.5 first:border-t-0"
+          className="w-full border-2 border-slate-800 text-gray-900 text-sm block p-2.5 first:border-t-0"
         />
       </li>
 
       <li>
         <input
           type="text"
-          id={name+0.5}
+          id={name + 0.5}
           name={name + 0.5}
           onChange={handleInputChange}
           defaultValue={formData[name + 0.5]}
-          className="w-full border-y-2 border-slate-800 text-gray-900 text-sm block py-2.5 first:border-t-0"
+          className="w-full border-y-2 border-slate-800 text-gray-900 text-sm block p-2.5 first:border-t-0"
         />
       </li>
     </ul>
