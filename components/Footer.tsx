adb7804c2d24a4be0e0dd62b5f55f86c8d3f5725
UNHOT <Serverings@protonmail.com>
2023-03-27 11:42:38 +0430

    Minor Fix.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,18 +3,34 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="text-sm flex justify-center my-4">
+    <div className="border-t-2 border-slate-800 py-4 flex flex-col text-sm items-center">
       <ul>
-        <li>
-          Made With Love By <i className="bi bi-github"></i>
+        <li className="flex items-center">
+          Made With
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            className="bi bi-suit-heart-fill fill-red-600 mx-1"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+            />
+          </svg>
+          By
           <Link
-            className="text-blue-600"
+            className="text-blue-600 ml-1"
             href={"https://github.com/UNHOTOfficial"}
           >
             UnHot
           </Link>
         </li>
       </ul>
+      <div>
+        <span>© 2023 UnHot™. All Rights Reserved.</span>
+      </div>
     </div>
   );
 }
