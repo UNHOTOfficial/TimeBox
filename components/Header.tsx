c97c30581805211bc139133150d27c6d4aae2b08
UNHOT <Serverings@protonmail.com>
2023-03-29 12:59:08 +0430

    Add label.

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,11 @@ export default function Header() {
       <span className="border-2 border-slate-900 px-4 py-1.5 font-bold  dark:border-slate-100">
         TimeBox
       </span>
+      <label className="sr-only" htmlFor="darkModeToggle">
+        Change Theme
+      </label>
       <button
+        id="darkModeToggle"
         type="button"
         className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
         onClick={handleDarkMode}
