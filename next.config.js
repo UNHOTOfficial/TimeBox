5bd559640a1b0a1d6225bb83048aab654eca447b
UNHOT <Serverings@protonmail.com>
2023-03-31 13:41:08 +0430

    Fix Pwa.

--- next.config.js
+++ next.config.js
@@ -7,12 +7,31 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-const withPWA = require("next-pwa")({
-  //disable: process.env.NODE_ENV === "development",
+const withPWAInit = require("next-pwa");
+
+const isDev = process.env.NODE_ENV !== "production";
+
+const withPWA = withPWAInit({
   dest: "public",
-  register: true,
-  skipWaiting: false,
-  runtimeCaching,
+  disable: isDev,
+
+  exclude: [
+    // add buildExcludes here
+    ({ asset, compilation }) => {
+      if (
+        asset.name.startsWith("server/") ||
+        asset.name.match(
+          /^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/
+        )
+      ) {
+        return true;
+      }
+      if (isDev && !asset.name.startsWith("static/runtime/")) {
+        return true;
+      }
+      return false;
+    },
+  ],
 });
 
 module.exports = withPWA(nextConfig);
